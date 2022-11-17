a809bdf991bc42439b885174ca25c798c382d61e
takaNoMeImer <uchihaimerak1@gmail.com>
2022-11-17 15:24:59 -0600

    Revisar fallos

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,67 @@
 <script>
-    /* Google authentication implementation */
-    import { auth, googleProvider } from './firebase' 
-    import { authState } from 'rxfire/auth'
+	import {auth,googleProvider} from "./firebase";
+	import {authState} from "rxfire/auth";
 
-    let user; 
+	import Chatroom from "./lib/Chatroom.svelte";
 
-    const unsubscribe = authState(auth).subscribe(usr => user = usr)
+	let user;
 
-    function login () {
-        auth.signInWithPopup(googleProvider)
-    }
+	const unsubscribe = authState(auth).subscribe(usr => user = usr);
 
-    function logout () {
-        auth.signOut()
-    }
+	function login(){
+		auth.signInWithPopup(googleProvider);
+	}
+
+	function logout(){
+		auth.signOut();
+	}
 </script>
 
-<div />
-
 <style>
+	main {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		width: 100%;
+		height: 100%;
+		max-width: 400px;
+		max-height: 500px;
+		background: #fff;
+		border: 1px solid #eee;
+		box-shadow: 0px 5px 10px rgba(0,0,0,0.05);
+	}
+	.login-form {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);	
+	}
+	.login-form button {
+		padding: 10px;
+		background: #fff;
+		color: #111;
+		font-size: 16px;
+		cursor: pointer;
+		outline: none;
+		border: 1px solid #bbb;
+	}
+	.login-form button i.fa {
+		padding-right: 10px;
+		border-right: 1px solid #ddd;
+		color: #555;
+	}
 </style>
+
+<main>
+	{#if user}
+		<Chatroom user={user} logout={logout}/>
+	{:else}
+		<div class="login-form">
+			<button on:click={login}>
+				<i class="fa fa-google"></i>
+				Sign In with Google
+			</button>
+		</div>
+	{/if}
+</main>
